8dd51682fb2543ab6bdcf78b504708dd7961e9ca
AhmeedDahy <ahmedss2235555@gmail.com>
2024-09-21 03:50:04 +0300

    v-2

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -83,7 +83,7 @@ function CreateOrder() {
             )}
           </div>
           {!position.latitude && !position.longitude && (
-            <span className="absolute right-[3px] top-[3px] md:right-[5px] md:top-[5px] z-88">
+            <span className="absolute right-[3px] top-[35px] md:right-[5px] md:top-[0px] z-88">
               <Button
                 type="small"
                 disabled={isLoadingAddress}
